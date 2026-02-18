5cbce7bcfefd57b450daee501d57fc228666417a
Paulo Sergio de Oliveira <149097641+master102121@users.noreply.github.com>
2026-02-18 13:42:04 -0300

    Update SaltCrypt.php

--- src/SaltCrypt.php
+++ src/SaltCrypt.php
@@ -2,36 +2,41 @@
 namespace Master102121\Encriptor;
 
 class SaltCrypt {
-  private $key = 'CUSTOM_MY_SALT_KEY';
-  private $plaintext = "String_to_be_encrypted";
+  private $key;
+  private $plaintext;
+  private $cipher = "AES-128-CBC";
   
-  public function __construct($key, $plaintext) {
+  public function __construct($key, $plaintext = "") {
     $this->key = $key;
     $this->plaintext = $plaintext;
   }
   
-  public function encrypt(){
-    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+  public function encrypt() {
+    $ivlen = openssl_cipher_iv_length($this->cipher);
     $iv = openssl_random_pseudo_bytes($ivlen);
-    $ciphertext_raw = openssl_encrypt($this->plaintext, $cipher, $this->key, $options=OPENSSL_RAW_DATA, $iv);
-    $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary=true);
-    return base64_encode($iv.$hmac.$ciphertext_raw);
+    
+    // Removido o "=" dentro da função para evitar o Deprecated
+    $ciphertext_raw = openssl_encrypt($this->plaintext, $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv);
+    
+    $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, true);
+    return base64_encode($iv . $hmac . $ciphertext_raw);
   }
   
-  
-  public function decrypt($ciphertext){
+  public function decrypt($ciphertext) {
     $c = base64_decode($ciphertext);
-    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
-    $iv = substr($c, 0, $ivlen);
-    $hmac = substr($c, $ivlen, $sha2len=32);
-    $ciphertext_raw = substr($c, $ivlen+$sha2len);
-    $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $this->key, $options=OPENSSL_RAW_DATA, $iv);
-    $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary=true);
+    $ivlen = openssl_cipher_iv_length($this->cipher);
     
-    if(hash_equals($hmac, $calcmac)){
+    $iv = substr($c, 0, $ivlen);
+    $hmac = substr($c, $ivlen, 32); // sha2len é 32
+    $ciphertext_raw = substr($c, $ivlen + 32);
+    
+    $original_plaintext = openssl_decrypt($ciphertext_raw, $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv);
+    $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->key, true);
+    
+    if (hash_equals($hmac, $calcmac)) {
       return $original_plaintext;
-    }else{
-      return 'Chave incorreta!';
     }
+    
+    return 'Chave incorreta!';
   }
 }
